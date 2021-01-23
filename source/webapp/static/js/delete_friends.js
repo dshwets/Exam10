6c3fe3368533e5a64cae5af9a44646a5ce7ae039
Daniil <dshwets@gmail.com>
2021-01-23 17:10:38 +0600

    Закончил второй этап.

--- source/webapp/static/js/delete_friends.js
+++ source/webapp/static/js/delete_friends.js
@@ -21,3 +21,26 @@ window.addEventListener('load', function() {
     for (let btn of btns) {btn.onclick = removeFriends}
 
 });
+
+async function addFriends(event) {
+    event.preventDefault();
+    let addtofriendBtn = event.target.parentElement;
+    let url = addtofriendBtn.href;
+
+    try {
+        let response = await makeRequest(url, 'POST');
+        let data = await response.text();
+        console.log(data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+window.addEventListener('load', function() {
+    const btns = document.getElementsByClassName('btn btn-success')
+    for (let btn of btns) {btn.onclick = addFriends}
+
+});
